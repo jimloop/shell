40da286f098b9a48c12c7677e086116b9fcbf3d5
lixiaofeng <1447539855@qq.com>
2019-10-12 19:40:56 +0800

    基本操作

--- test.sh
+++ test.sh
@@ -58,4 +58,61 @@ else
 fi
 
 #文件测试
-#
+# -e 文件名   如果文件存在则为真  -r文件名   如果文件存在且可读则为真   -w文件名   如果文件存在且可写则为真   -x文件名    如果文件存在且可执行则为真
+# -s文件名   如果文件存在且至少一个字符串则为真   -d文件名   如果文件存在且为目录则为真    -f文件名  如果文件存在且为普通文件则为真   -c文件名   如果文件存在且为字符型特殊文件则为真
+# -b文件名 如果文件存在且为块特殊文件则为真
+if test -e ./bash
+then
+  echo '文件已存在！'
+else
+  echo '文件不存在！'
+fi
+
+#shell 还提供了 与(-a)、或(-o)、非(!)三个逻辑操作符用于将测试条件连接起来，其优先级为：“!”最高  “-a”次之  "-o"最低。
+if test -e ./myfile -o -e ./bash
+then
+  echo '至少有一个文件存在'
+else
+  echo '两个文件都不存在'
+fi
+
+#shell函数
+funWithReturn(){
+  echo "这个函数会对输入的两个数字进行相加运算..."
+  echo "请输入第一个数字："
+  read aNum
+  echo "请输入第二个数字："
+  read bNum
+  # shellcheck disable=SC2004
+  echo "两个数字分别为$aNum和$bNum，和为：$(($aNum+$bNum))"
+  # shellcheck disable=SC2004
+  return $(($aNum+$bNum))
+}
+
+funWithReturn
+echo "function 'funWithReturn'的执行结果为：$? !"
+
+#函数参数
+funWithParams(){
+  echo "第一个参数为：$1"
+  echo "第二个参数为：$2"
+  echo "第三个参数为：$3"
+  echo "第十个参数为：${10}"
+  echo "一共有$#个参数"
+  echo "作为一个字符串输出所有参数：$*"
+}
+
+funWithParams 1 2 3 4 5 6
+#参数处理的特殊字符： $#  传递到脚本的参数个数  $*  以一个单字符串显示所有向脚本传递的参数   $$脚本运行的当前进程的ID号   $!   后台运行的最后一个进程的ID号
+#                    $@   与$*相同，但在返回时加引号，并在引号中返回每个参数   $-   显示shell使用的当前选项，与set命令功能相同   $?  显示最后命令的退出状态。0表示没有错误，其他任何值表示有错误
+
+#shell输入输出重定向
+#一个命令通常从一个叫标准输入的地方读取输入，默认情况下，这恰好是你的终端；同样，一个命令通常将输出写入到标准输出，默认情况下，这也是你的终端输；
+(重定向命令列表如下:
+ command > file  将输出重定向到file
+ command < file  将输入重定向到file
+ command >> file 将输出以追加的方式重定向到file
+ n > file   将文件描述符为n的文件重定向到file
+ n >> file  将文件描述符为n的文件以追加的方式重定向到file
+
+)
